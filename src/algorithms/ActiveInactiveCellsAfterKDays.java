88322960b0bbe6ec487105a91834546756182b93
DESKTOP-5LKLR5J\DNyaika <davie2086@gmail.com>
2019-02-07 19:08:50 -0600

    added some exaple to the question

--- src/algorithms/ActiveInactiveCellsAfterKDays.java
+++ src/algorithms/ActiveInactiveCellsAfterKDays.java
@@ -7,6 +7,14 @@ and false (or 0) means inactive. Given a number k, the task is to find count of 
 After every day, status of i’th cell becomes active if left and right cells are not same and inactive if left and right cell are same (both 0 or both 1).
 Since there are no cells before leftmost and after rightmost cells, 
 the value cells before leftmost and after rightmost cells is always considered as 0 (or inactive).
+*****************************
+Example
+******************************
+*
+*Input  : cells[] = {1, 0, 1, 1}, k = 2
+Output : Active cells = 3, Inactive cells = 1
+After 1 day,  cells[] = {0, 0, 1, 1}
+After 2 days, cells[] = {0, 1, 1, 1}
  */
 
 public class ActiveInactiveCellsAfterKDays {
